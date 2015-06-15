b0bc181a7923ab4148d3a71feb68000439b9e15f
Dillon Good <dilloncgood@gmail.com>
2015-06-15 15:56:05 -0600

    Tests passing

--- house.js
+++ house.js
@@ -1,7 +1,13 @@
-var House = function(){
-
-
+var House = function(numOfBedrooms) {
+  this.numOfBedrooms = numOfBedrooms;
+  this.rooms = {}
 };
 
+House.prototype.getRooms = function() {
+  return this.rooms
+}
 
+House.prototype.addRoom = function(room) {
+  return this.rooms[room.name] = []
+}
 module.exports = House;
--- room.js
+++ room.js
@@ -1,7 +1,23 @@
-var Room = function(){
+var Room = function(name, width, length){
+  this.contents = [];
+  this.name = name;
+  this.width = width;
+  this.length = length;
+}
 
+Room.prototype.addFurniture = function(furniture) {
+  if (this.contents.indexOf(furniture) === -1) {
+    this.contents.push(furniture)
+  } else {
+    return "sorry, you can't have two of those"
+  }
+}
 
-};
-
+Room.prototype.removeFurniture = function(furniture) {
+  var furny = this.contents.indexOf(furniture)
+  if (this.contents.indexOf(furniture) !== -1) {
+    this.contents.splice(furny, 1)
+  }
+}
 
 module.exports = Room;
